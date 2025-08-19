7498e23038deafe04bd8f71ca3a4f7f63bbb3aa8
Ngozi Nwadialo <jonathanengee@gmail.com>
2025-08-19 10:48:52 +0100

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,7 +3,7 @@ variable "instance_name" {
 }
 
 variable "key_name" {
-  default = "terra"                  # Names of key in aws
+  default = "mytest_keypair"                  # Names of key in aws
 }
 
 
